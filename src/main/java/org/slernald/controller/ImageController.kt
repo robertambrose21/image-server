c4bb5d0ac1200affd66641ada8867096cb1f467d
slernald <slernald@gmail.com>
2020-05-05 19:56:38 +1200

    feat: added ability for REST server to receive images

--- src/main/java/org/slernald/controller/ImageController.kt
+++ src/main/java/org/slernald/controller/ImageController.kt
@@ -5,9 +5,8 @@ import org.apache.tomcat.util.http.fileupload.IOUtils
 import org.slernald.services.images.impl.ImageServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 import javax.servlet.http.HttpServletResponse
@@ -34,6 +33,11 @@ class ImageController {
     @GetMapping("/images")
     fun getAllImageIds(): List<Long?> {
         return imageService.getAllImageIds();
+    }
+
+    @PostMapping("/images")
+    fun saveImage(@RequestParam("data") file: MultipartFile) {
+        imageService.saveImage(file);
     }
 
 }
